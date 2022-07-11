07a3a57687540a11ea2ecb6e3673943af08d15a5
Zachary Bedrosian <zachary@cleancult.com>
2022-07-11 16:33:36 -0400

    Added in prisma model

---
+++ pages/api/graphql.js
@@ -0,0 +1,42 @@
+import { gql, ApolloServer } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type User {
+    id: ID
+  }
+
+  type Query {
+    getUser: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: () => {
+      return {
+        id: "Foo",
+      };
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
