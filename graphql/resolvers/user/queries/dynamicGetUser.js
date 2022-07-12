c1415538dc58c2d73ddce9344408f10a16264fdd
Zachary Bedrosian <zachary@cleancult.com>
2022-07-12 08:12:11 -0400

    Finalized dynamic get user

--- graphql/resolvers/user/queries/dynamicGetUser.js
+++ graphql/resolvers/user/queries/dynamicGetUser.js
@@ -32,7 +32,7 @@ export default {
                 dateOfBirth: true,
                 childCarePlans: {
                   where: {
-                    active: true
+                    active: true,
                   },
                   select: {
                     id: true,
@@ -55,22 +55,22 @@ export default {
                                 id: true,
                                 image: true,
                                 description: true,
-                                level: true
-                              }
-                            }
-                          }
-                        }
-                      }
+                                level: true,
+                              },
+                            },
+                          },
+                        },
+                      },
                     },
                     therapist: {
                       select: {
                         firstName: true,
                         lastName: true,
-                        email: true
-                      }
-                    }
-                  }
-                }
+                        email: true,
+                      },
+                    },
+                  },
+                },
               },
             },
             organizations: {
@@ -89,6 +89,162 @@ export default {
             },
           },
         });
+      } else if (context.user.role === "THERAPIST") {
+        userObject = await prisma.user.findUnique({
+          where: {
+            id: context.user.id,
+          },
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            role: true,
+            email: true,
+            phoneNumber: true,
+            patientCarePlans: {
+              where: {
+                active: true,
+              },
+              select: {
+                id: true,
+                level: true,
+                allVideoStatus: true,
+                weeklyVideoStatus: true,
+                assignments: {
+                  select: {
+                    id: true,
+                    createdAt: true,
+                    dateStart: true,
+                    dateDue: true,
+                    description: true,
+                    videos: {
+                      select: {
+                        id: true,
+                        contentfulID: true,
+                        medals: {
+                          select: {
+                            id: true,
+                            image: true,
+                            description: true,
+                            level: true,
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            ownedOrganization: {
+              select: {
+                id: true,
+                createdAt: true,
+                phoneNumber: true,
+                organizationType: true,
+                stripeSubscriptionID: true,
+                stripeSubscriptionID: true,
+                active: true,
+                organizationUsers: {
+                  select: {
+                    id: true,
+                    active: true,
+                    user: {
+                      select: {
+                        id: true,
+                        role: true,
+                        firstName: true,
+                        lastName: true
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            organizations: {
+              where: {
+                active: true,
+              },
+              select: {
+                organization: {
+                  select: {
+                    id: true,
+                    name: true,
+                    organizationType: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+      } else if (context.user.role === "SCHOOL_ADMIN") {
+        userObject = await prisma.user.findUnique({
+          where: {
+            id: context.user.id,
+          },
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            role: true,
+            email: true,
+            phoneNumber: true,
+            ownedOrganization: {
+              select: {
+                id: true,
+                createdAt: true,
+                phoneNumber: true,
+                organizationType: true,
+                stripeSubscriptionID: true,
+                stripeSubscriptionID: true,
+                active: true,
+                organizationUsers: {
+                  select: {
+                    id: true,
+                    active: true,
+                    user: {
+                      select: {
+                        id: true,
+                        role: true,
+                        firstName: true,
+                        lastName: true
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            organizations: {
+              where: {
+                active: true,
+              },
+              select: {
+                organization: {
+                  select: {
+                    id: true,
+                    name: true,
+                    organizationType: true,
+                    organizationUsers: {
+                      select: {
+                        id: true,
+                        active: true,
+                        user: {
+                          select: {
+                            id: true,
+                            role: true,
+                            firstName: true,
+                            lastName: true
+                          }
+                        }
+                      }
+                    }
+                  },
+                },
+              },
+            },
+          },
+        });
+      } else {
+        return null;
       }
 
       return userObject;
