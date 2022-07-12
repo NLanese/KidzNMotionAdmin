e510d3b045e9ec45fd67c650b907b5f39b101ff4
Zachary Bedrosian <zachary@cleancult.com>
2022-07-11 21:45:37 -0400

    Removed operations

--- graphql/operations.js
+++ graphql/operations.js
@@ -1,98 +1,24 @@
 import { gql } from "@apollo/client";
 
-/////////////////////////////
-///                       ///
-///     Landing Page      ///
-///                       ///
-/////////////////////////////
+//ALL NEEDS TO BE CHANGED FOR 
+// JUST PLACEHOLDERS FOR NOW!!
 const DYNAMIC_SIGN_IN = gql`
-  mutation Mutation($email: String!, $password: String!) {
-    dynamicSignIn(email: $email, password: $password) {
-      id
-      role
-      token
-      firstname
-      signUpToken
-      lastname
-      email
-      dsp {
-        id
-        paid
-        createdAt
-        accountStanding
-      }
-    }
-  }
+  
 `;
 const OWNER_SIGN_UP = gql`
-  mutation OwnerSignUp(
-    $email: String!
-    $password: String!
-    $firstname: String!
-    $lastname: String!
-    $phoneNumber: String!
-  ) {
-    ownerSignUp(
-      email: $email
-      password: $password
-      firstname: $firstname
-      lastname: $lastname
-      phoneNumber: $phoneNumber
-    ) {
-      id
-      role
-      signUpToken
-      token
-      firstname
-      lastname
-      email
-    }
-  }
+ 
 `;
 const MANAGER_SIGN_UP = gql`
-  mutation Mutation(
-    $email: String!
-    $password: String!
-    $firstname: String!
-    $lastname: String!
-    $phoneNumber: String!
-    $signupToken: String!
-  ) {
-    managerSignUp(
-      email: $email
-      password: $password
-      firstname: $firstname
-      lastname: $lastname
-      phoneNumber: $phoneNumber
-      signupToken: $signupToken
-    ) {
-      id
-      role
-      token
-      firstname
-      lastname
-      email
-    }
-  }
+  
 `; 
 
 
 const REQUEST_PASSWORD_RESET = gql`
-  mutation Mutation($email: String!) {
-    dynamicForgotPassword(email: $email)
-  }
+ 
 `; 
 
 const RESET_PASSWORD = gql`
-  mutation Mutation(
-    $password: String!
-    $token: String!
-  ) {
-    dynamicResetPassword(
-      password: $password
-      token: $token
-    )
-  }
+
 `; 
 
 
