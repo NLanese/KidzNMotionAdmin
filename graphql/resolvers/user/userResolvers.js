99814a589e2e11bfa1ab26cc8cc47d8184f0fb58
Zachary Bedrosian <zachary@cleancult.com>
2022-07-13 11:09:42 -0400

    Import fix

--- graphql/resolvers/user/userResolvers.js
+++ graphql/resolvers/user/userResolvers.js
@@ -4,10 +4,10 @@ import GraphQLJSON from 'graphql-type-json';
 import getUser from './queries/getUser.js';
 
 // USER MUTATIONS
+import signUpUser from './mutations/signUpUser.js'
 import devCreateUser from './mutations/devCreateUser.js';
 import loginUser from './mutations/loginUser.js';
 import logoutUser from './mutations/logoutUser.js';
-import signUpUser from './mutations/signUpUser.js';
 
 export default {
     Query: {
