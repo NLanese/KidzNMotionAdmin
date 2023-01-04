0c5d460c0cd6ef08d3845218b649b0dbf8888ccb
Zachary Bedrosian <zachary@cleancult.com>
2023-01-04 10:02:04 -0500

    Added in final touches

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ import {
 
 import BasicLink from "@common/BasicLink";
 
-
 const { Text, Title, Link } = Typography;
 
 const IndexWrapper = styled.div`
@@ -39,18 +38,19 @@ function Index() {
     <IndexWrapper>
       <PageHeader title="Welcome To Kidz-N-Motion " />
       <Row gutter={[16, 3]}>
-     
         <Col xs={24} sm={24}>
           <ContentCard>
             <Space direction="vertical">
               <Title style={{ margin: "0px" }} level={5}>
-                <InfoCircleOutlined /> Learn how to make the most out of Kidz-N-Motion
+                <InfoCircleOutlined /> Learn how to make the most out of
+                Kidz-N-Motion
               </Title>
               <Text>
-              We've got the tutorials for the functionality and features to answer your questions.
+                We've got the tutorials for the functionality and features to
+                answer your questions.
               </Text>
               <BasicLink href="/support">
-                <Link type="link">Learn more</Link>
+                <Link type="link">Learn more about our tutorial</Link>
               </BasicLink>
             </Space>
           </ContentCard>
