4ace891579d71ba32b958e7cc588f63c794b220b
Zachary Bedrosian <zachary@cleancult.com>
2023-01-04 11:39:00 -0500

    Added in new cert

--- utils/firebase.js
+++ utils/firebase.js
@@ -6,18 +6,18 @@ try {
   admin.initializeApp({
     credential: admin.credential.cert({
       type: "service_account",
-      project_id: "kidznmotion-7f5cf",
-      private_key_id: "4217ad818a7db980a382f04bb5c6bf1ffe9b2c0f",
+      project_id: "kidznmotionios",
+      private_key_id: "7e9311f09fde555af83cc0b32cc91c50764dfba9",
       private_key:
-        "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDULazBUK1XLq+m\n+3QvnoOLB9xM/3EIGtccizX7rBRX4h3wOOGIKF9mkcVKnDEuTwhOb7E0N0BIxKPF\n2rMjg6qEcMqZ3RRcVDeA9/+Gy+EXJbijcOYExWrQ7pFRN/Q2oFjtAkM9sM5XEqeQ\n0PLuwy1z9e5jdXPDgkjbZjGznbAXcaZM/mLttdKtqjEU3EdXQ4r0MIVecJY0D2I+\nnkssCYWLu6CLf3ar57Jp4Lp+ADgDDlZEOAd4g5H9gubc20WMYsK6AkdnWtlSsZoS\nl1M4LFbZyko37IcwD2kf8XEqdTgQrehEuvqb4HJxfOrCv3Lc996IBP91zMA6paXQ\nj3/eLmm7AgMBAAECggEAKvjJKSnGwYZyCraYlGBdMdY4tttxL/wHnv9OwpbaU+Y2\nvAU5IBNVJvfU5kR17XQSMZiLYhDikxpINPUTHNk4urQBRGIzEnP3/cW8WbeG8eIw\n3+EopMS62m/GvJVBf6K//9had+8MCzR6/WhkGxkqZnbjff82n90OFmk/HaqNFGIQ\nrcVJ5llbVi/k+nbkbeiqOPAgaBqTxDgrEUhaBhZYSENrOvZTrNZX16ms8qyNtX3j\nTC6be/BEag7dfv5AH9VSX1UPd9RRVEgPCA6fS/krTl0m/iVQN0/NA3fVfyvyRSXw\nQ1gqa8zMVtt5ksujujHN25jEYwJiZze3xiO61VzrKQKBgQDpqlQXC3TVEUxhYbdr\nQMqlkF0myk8GAGl+GJdzevszrwjPLA59wruHIaio3rhL7V8uQV8dr8MJkP/+9Uwr\nruDsfOPx8KVjYXlpbAzeqdnhnfjj2oYcLV+eoO4qzuVGGp4kg+KhICZ50vSPQPO2\nnYb6g3v18+pz+dcOm/baQtIgdwKBgQDodZJ7pWXHDk+rqgr6bjVPl+UCumqqILWM\nBCti0lHCn4F4s61oBYs0IoWNpuPGeUC9nwSpx0qB3CQFZcKxNf0UHnZxJKBWjOEt\n2gZ3NMMl6gME5obVO5V1JQ9UfsDFox9NYcAmBohhgpbGqXJFWE+yP7JU3fdBUy94\nhauWaB513QKBgFXJeBgB18IGGNHD63BQGFZSuzBrpDmi2xg1dR0rsKBYCdbt6OAZ\nJ3pETES7iFQv8PrteztN9UBE4Qe9DVsYWHHapGGGJWJoPggLaGozV8/yjJcy+nvL\n2MhHc0l6LkjfquHjHchkIlwlrC4P/ao1CYJd1ZwGEjTELEWr0yRs1fKfAoGAUFJX\nxBuZeFdslf9SfcfY6eCtQ+0P8CsPMAs8xo+vqpUc0o0bLOdFe0U2aoSxDaJWCW9J\n2DRBfW+IjsjjpgHmqa4A9QVX1bAmGobMVyJHD/G1C1twWXuWH/0nN+X4TBIaPc25\n7CmztCXAXXKtRmLGXuv9fa+YwNyyeW2OpZDifY0CgYEA3MPi7memeJMYM5eZpZa7\n6NpXoHSUH852MJX1BvPmaUNVsz/+zKiuFqbIM9gZCMCHDQjiR2ilXqUHSUAuQY/Z\nZuBZxTlWbPBLY3A9rKHptuYzF+D0kZyQI0zltAKLeKOVQ8w2CZMlV99UhvEkdAmf\noablGfsoxKkliCyjUwlJ9pM=\n-----END PRIVATE KEY-----\n",
+        "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDCu8qumUTXFysv\nILZ8/AGZuAde2rDM/cyqoh25YJGdiQJZJckB9Ifs+kvIodWapuusW1/TXAeoOrIo\n0s1MXgzJapXObAQ8MLVB7W2JcWo7fep1pO0ziwuWkNjiMUHT18mYKLaonZN5cdho\nC0RKg3h1WR12qKJAVcE+DNmR6mDpPcozEMjvQX+CzP5UneLCBY/dTQPgXceJPC5e\n2OhhePdEXZLTWYJ9vEhN5Icc7aqt4bjokV39Aii8/ssV59OrqcOLPTxOrlXblAV4\nJBbjeiiDTTpiZ451esKPIdsbzdEHV5MFiOzjuxfDr6UE6kvuGVPADAOsydJejyPH\nKUh39lFhAgMBAAECggEAQgwXjmiFpL5C81UREAsZL93vAgkCdkSOqkEZ+8n5GYli\ntoHOT0902rd6BK5/GFkNDgRf1/wazMA7tQTqhMBRbe8sqDqSpSeQErBIupkxAsSO\nRSCPIzBB2et+5Bj4zrUdtvH4IfBwGHy4fXF01CELNiHnkFtZzKBJTpZlJMxt2i12\nNb3A+Pnolz9acs9ktooIb6Or3KHNje09qGQI1RATIwunnLnCaB9RFQ1M+3FUZk9G\nRlGKaGxFG7pO0gi0t7qKeYYYyI4QekwkBIccN2OxQYlz5iKR9Lv1914Amxurs7O8\nSjojkd3rfHOBZuc23Jrom/Lc0o8ewN/Cjd5n+In6kQKBgQD94GZvyJckOYXhvA9J\nwptuKkxFHRgFVYoCYPywzw2FitrbE3BgQfHsYWfPGSkKFhZ6+UvontwVrHH2swXD\nSb/pbVu2uBdhejSLPzTgwQ28rCJ8Lod3D/21nf9MggKo88k3xFon9lx/kzewa7wR\nn+tM45miwT6BpJU8GArQcq5FqwKBgQDEXME2efglvewzML6bJAHMo25BLYhBcebV\nFqvgrM5lY3gvA+/2N4hInfW3UKzP3OZSvTKP7Si6kWi6oVtUU+rXE1MlIygOlsI4\necMdpMBB81RTa/UwgvMvFgksgld+Uqz95fCmG47/E/bOMipWdHztZg7c323REnGk\nc7ntXjdhIwKBgDTMvME22KhCBmQiNpPCk4zf4yjkmiBR3/gKr8Md7NP/QvEZI+AB\nFw1wXKSnuO9OFLLSskTY9TDUwobqSKKK7gzgH5jqxIITZmYnaRntTKwWqL1m9+l9\nO+jPyU7iIyd8PDcK8S0qgaZOueBJJ74KomdFVqiQxC02S4tJ/MXJdBEtAoGBAJK2\nUs4nF1brrj0ZIHC92HJTcZ9VK8bqGXQAum7l7lgml7dZE8M8/qBzFRHgPx4XiCct\noUeUBKhQ2JUI1joNcRs9HxRSTi2ISpjzsOrHG7aq3chukwWp8ZSuvC/7Ou/xSbty\nP/l8Ab1n5zFsneF2q5icdmJktaJOS4Z5yOMnT+1NAoGBAKQR8CCJFMIWWP0siJrz\ng80PmyFaYtR4gm14NT4+t3bU9xh/jdGm0Q4i3xhlTYOQgHVSGpeRzcjHMb5PIFE2\nMgVGuQbfIegu47csZCA4gOokWGQbo8T4hel663PTbfI6xIM8VTZo3dhEmMG6Bpov\n4EeO6G5ijZ6cGCSLlULKnPEp\n-----END PRIVATE KEY-----\n",
       client_email:
-        "firebase-adminsdk-duffz@kidznmotion-7f5cf.iam.gserviceaccount.com",
-      client_id: "105738133117841515457",
+        "firebase-adminsdk-r0ewz@kidznmotionios.iam.gserviceaccount.com",
+      client_id: "101508316331295813214",
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
       token_uri: "https://oauth2.googleapis.com/token",
       auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
       client_x509_cert_url:
-        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-duffz%40kidznmotion-7f5cf.iam.gserviceaccount.com",
+        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-r0ewz%40kidznmotionios.iam.gserviceaccount.com",
     }),
   });
   // console.log("Initialized.");
