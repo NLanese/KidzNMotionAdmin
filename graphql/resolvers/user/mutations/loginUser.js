a456ef90d0bd316a70bacf0f9012d942e0922055
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-03-02 03:07:49 -0500

    Cleaned password bypass

--- graphql/resolvers/user/mutations/loginUser.js
+++ graphql/resolvers/user/mutations/loginUser.js
@@ -110,8 +110,8 @@ export default {
         let decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
 
         // If the passwords match (JWT Actions)
-        // if (decryptedPassword === password) {
-          if (true){
+        if (decryptedPassword === password) {
+          
           // Create the client string
           const jwtTokenString = makeRandomString(60);
 
