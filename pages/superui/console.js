d8755459ed189bde04c0113f046c2c77aef1564a
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-10-20 12:46:34 -0400

    Update console.js

--- pages/superui/console.js
+++ pages/superui/console.js
@@ -1,17 +1,80 @@
+// React
 import React, { useState, useEffect } from "react";
 
+// Mutations and Queries
 import { useMutation, useQuery } from "@apollo/client";
+import {SUPER_GET_ALL_CLIETS, SUPER_GET_ALL_THERAPISTS} from "../../graphql/operations"
 import client from "@utils/apolloClient";
+
+// Ant Design
+import { Col, Row, Button, message, Divider, Spin, DatePicker } from "antd";
+
 
 
 
 function Console() {
-    return (
-      <div>
-        <h1>Super console</h1>
-        {/* Your SuperUI page content goes here */}
-      </div>
-    );
+  /////////////////////////
+  // State and Constants //
+  /////////////////////////
+
+  const [clients, setClients] = useState([])
+
+  const [therapists, setTherapists] = useState([])
+
+  // This determines which inforomation is displayed on-screen. | None | Clients | Therapists | 
+  const [consoleState, setConsoleState] = useState("None")  
+
+
+//////////////////////////
+// Muations and Queries //
+//////////////////////////
+
+const { clientsLoading, clientsError, clientsData } = useQuery(SUPER_GET_ALL_CLIETS);
+
+const { therapistsLoading, therapistsError, therapistsData } = useQuery(SUPER_GET_ALL_THERAPISTS);
+
+
+
+////////////////
+// Renderings //
+////////////////
+
+  const renderAllClients = () => {
+
+  }
+
+  /////////////////
+  // MAIN RETURN //
+  /////////////////
+  const MAIN = () => {
+
+    // Still Loading Queries
+    if (clientsLoading || therapistsLoading){
+      return(
+        <div>
+          <h1>Super console</h1>
+          <p>Please wait as the console data is still loading</p>
+        </div>
+      )
+    }
+
+    // Loading Complete, WITH ERROR
+    else if (clientsError || therapistsError){
+      return (
+        <div>
+          <h1>Super console</h1>
+          <p>There was an error with one of the queries!</p>
+        </div>
+      );
+    }
   }
   
-  export default Console;
+  return (
+    <div>
+      <h1>Super console</h1>
+      
+    </div>
+  );
+}
+  
+export default Console;
