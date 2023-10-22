95b7e90c7d172a4d3cdbfaeacf0b75e122912b47
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-10-22 14:57:56 -0400

    Testing on the GraphQL IDE and adding

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ to run locally...
 - nvm use 16.10
 - npm run dev
 
+to access the graphQL Playground, go to these urls
+- locaolhost:3000/api/graphql (local)
+- kidz-n-motion.app/api/graphql
+
 ===== TO POST UPDATES =====
 - Updating the master branch in this repo will automatically trigger an update on the live site
--- graphql/resolvers/superuser/mutations/setTherapist.js
+++ graphql/resolvers/superuser/mutations/setTherapist.js
@@ -8,79 +8,119 @@ export default {
 
           console.log("Inside superSetTherapist")
 
-            // Security //
-            if (!context.user) throw new UserInputError("Login required");
-            if (
-                context.user.email.toLowerCase() !== "nlanese21@gmail.com" ||
-                context.user.email.toLowerCase() !== "ostrichdeveloper@gmail.com" 
-            ){
-                throw new UserInputError("Acccess Denied! Super class actions are restricted to Super Users only.")
-            }
-            if (superUserKey !== process.env.SUPER_USER_SECRET_KEY){
-                throw new UserInputError("Acccess Denied! Super Key was incorrect.")
-            }
+            // // Security //
+            // if (!context.user) throw new UserInputError("Login required");
+            // if (
+            //     context.user.email.toLowerCase() !== "nlanese21@gmail.com" ||
+            //     context.user.email.toLowerCase() !== "ostrichdeveloper@gmail.com" 
+            // ){
+            //     throw new UserInputError("Acccess Denied! Super class actions are restricted to Super Users only.")
+            // }
+            // if (superUserKey !== process.env.SUPER_USER_SECRET_KEY){
+            //     throw new UserInputError("Acccess Denied! Super Key was incorrect.")
+            // }
 
             console.log("Passed authorization")
 
+            // IF NO CHILD CARE PLAN
             if (childCarePlanID === "false"){
 
               console.log("No current Plan")
 
-              // Finds Care Plan and updates the connected Therapist
-              let newPlan = await prisma.childCarePlan.create({
-                data: {
-                  child: {
-                    connect: {
-                      id: childID
-                    },
-                  },
-                  therapist: {
-                    connect: {
-                      id: therapistID
-                    },
-                  },
-                  level: parseInt(1),
-                },
-              });
+              /////////////////////
+              // CREATE NEW PLAN //
+              /////////////////////
 
-              console.log("New Plan Created")
-      
-              // Create the chat room for therapist + guardian
-              await prisma.chatroom.create({
-                data: {
-                  users: {
-                    connect: [
-                      {
-                        id: guardianID
-                      },
-                      {
+                // Finds Care Plan and updates the connected Therapist
+                let newPlan = await prisma.childCarePlan.create({
+                  data: {
+                    child: {
+                      connect: {
                         id: childID
                       },
-                    ],
-                  },
-                },
-              });
-
-              console.log("First chat made")
-
-              // Create chat rooms for child and therapist
-              await prisma.chatroom.create({
-                data: {
-                  users: {
-                    connect: [
-                      {
+                    },
+                    therapist: {
+                      connect: {
                         id: therapistID
                       },
-                      {
-                        id: guardianID
-                      },
-                    ],
+                    },
+                    level: parseInt(1),
                   },
-                },
-              });
+                });
 
-              console.log("Second chat amde")
+                console.log("New Plan Created")
 
+              /////////////////////////
+              // CREATE NEW CHATROOM //
+              /////////////////////////
+        
+                // Create the chat room for therapist + guardian
+                await prisma.chatroom.create({
+                  data: {
+                    users: {
+                      connect: [
+                        {
+                          id: guardianID
+                        },
+                        {
+                          id: childID
+                        },
+                      ],
+                    },
+                  },
+                });
+
+                console.log("First chat made")
+
+                // Create chat rooms for child and therapist
+                await prisma.chatroom.create({
+                  data: {
+                    users: {
+                      connect: [
+                        {
+                          id: therapistID
+                        },
+                        {
+                          id: guardianID
+                        },
+                      ],
+                    },
+                  },
+                });
+              
+              //////////////////////
+              // UPDATE THE USERS //
+              //////////////////////
+
+                await prisma.user.update({
+                  where: {
+                    id: childID,
+                  },
+                  data: {
+                    childCarePlans: {
+                      connect: {
+                        id: newPlan.id
+                      },
+                    },
+                  },
+                });
+                
+                await prisma.user.update({
+                  where: {
+                    id: therapistID,
+                  },
+                  data: {
+                    patientCarePlans: {
+                      connect: {
+                        id: newPlan.id
+                      },
+                    },
+                  },
+                });
+
+              //////////////////////
+              // RETURNS NEW PLAN //
+              //////////////////////
               return newPlan
           }
 
