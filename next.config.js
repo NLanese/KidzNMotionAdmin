5f30da5ab8103b347ab93f0628ed9eba7a8d4466
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2024-01-01 13:24:00 -0500

    Updated packages and next.js, still facing compilation issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
-const webpack = require('webpack'); // Add this line
-const withAntdLess = require('next-plugin-antd-less');
+const webpack = require('webpack');
 const { withSentryConfig } = require("@sentry/nextjs");
+const withAntdLess = require('next-plugin-antd-less');
 
 const moduleExports = withAntdLess({
   webpack: (config, { isServer }) => {
@@ -19,20 +19,29 @@ const moduleExports = withAntdLess({
 
     // Set API_URL environment variable for local development
     if (!isServer) {
+      const apiUrl = process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api/graphql'
+        : process.env.API_URL;
+
       config.plugins.push(
         new webpack.DefinePlugin({
-          'process.env.API_URL': JSON.stringify(
-            process.env.NODE_ENV === 'development' ? 
-            'http://localhost:3000/api/graphql' 
-            : 
-            process.env.API_URL
-          ),
+          'process.env.API_URL': JSON.stringify(apiUrl),
         })
       );
     }
+
+    // Add the following lines to handle NEXT_RUNTIME
+    const nextRuntime = process.env.NEXT_RUNTIME || 'default_runtime_value';
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_RUNTIME': JSON.stringify(nextRuntime),
+      })
+    );
+
     return config;
   },
-  lessVarsFilePath: './styles/variables.less', // optional 
+  lessVarsFilePath: './styles/variables.less',
   images: {
     formats: ['image/webp'],
     domains: ["images.ctfassets.net", "images.contentful.com", "cdn.shopify.com"],
