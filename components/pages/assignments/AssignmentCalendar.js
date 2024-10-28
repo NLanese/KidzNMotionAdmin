8ef85bcf02942c15bee1f685aceb282616020784
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2024-10-27 22:04:19 -0400

    Calendar seemed to not work with fully formatted dates, either that or moment updated

--- components/pages/assignments/AssignmentCalendar.js
+++ components/pages/assignments/AssignmentCalendar.js
@@ -14,14 +14,42 @@ const CalenderContainer = styled.div`
 `;
 
 function AssignmentCalendar({ router, assignments }) {
+
+
   const dateCellRender = (value) => {
-    return assignments.map((assignmentObject) => {
-      if (moment(assignmentObject.assignmentDateTime).isSame(value, "date")) {
+    return assignments.map((assignmentObject, index) => {
+      console.log(moment(assignmentObject.dateStart).isBefore(value, "date"))
+      console.log(moment(assignmentObject.dateDue).isBefore(value, "date"))
+      if (
+
+        // IF Due Date is after Current Date AND Start Date is before current date
+        (
+          moment(assignmentObject.dateStart).isBefore(value, "date") && 
+          moment(assignmentObject.dateDue).isAfter(value, "date"
+        )
+
+        ||
+
+        // IF Due Date is the Current Date OR Start Date is the current date
+        (
+          moment(assignmentObject.dateStart).isSame(value, "date") || 
+          moment(assignmentObject.dateDue).isSame(value, "date")
+        )
+      )) {
         return (
           <li key={assignmentObject.id}>
             <Badge status="success" text={assignmentObject.title } />
           </li>
         );
+      }
+      else{
+        console.log("=-=-=-=-=-=-")
+        console.log("START::: ", moment(assignmentObject.dateStart))
+        console.log("DUE::: ", moment(assignmentObject.dateDue))
+        console.log(value.$d)
+        console.log("AFTER ", assignmentObject.dateStart, "? ", moment(assignmentObject.dateStart).isBefore(value, "date"))
+        console.log("BEFORE ", assignmentObject.dateDue, "? ", moment(assignmentObject.dateDue).isAfter(value, "date"))
+        console.log("=-=-=-=-=-=-")
       }
     });
 
