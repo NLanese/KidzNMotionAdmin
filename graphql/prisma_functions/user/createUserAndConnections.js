db405d25784f006dd932775cc7e60a45455a4f3a
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2026-04-08 03:53:26 -0400

    Update createUserAndConnections.js

--- graphql/prisma_functions/user/createUserAndConnections.js
+++ graphql/prisma_functions/user/createUserAndConnections.js
@@ -25,6 +25,7 @@ export default async function createUserAndConnections(
 
         if (organizationInviteKey){
             addUserToOrgByIds(organizationInviteKey, guardianUser.id)
+            addUserToOrgByIds(organizationInviteKey, childUser.id)
         }
     }
 }
