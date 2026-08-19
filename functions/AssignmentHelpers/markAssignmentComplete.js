e82c8ca79074acfee5d6f54158ebe48f84556850
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2026-08-19 00:52:28 -0600

    Update markAssignmentComplete.js

--- functions/AssignmentHelpers/markAssignmentComplete.js
+++ functions/AssignmentHelpers/markAssignmentComplete.js
@@ -1,10 +1,21 @@
-export default function markAssignmentComplete(assignment, prisma){
+import { createNotification } from "@helpers/api/notifications";
+
+
+export default async function markAssignmentComplete(assignment, prisma, childUser){
     await prisma.assignment.update({
-                where: {
-                  id: assignment.id
-                },
-                data: {
-                  completed: true
-                }
-              })
+        where: {
+            id: assignment.id
+        },
+        data: {
+            completed: true
+        }
+    })
+    createNotification(
+        (childUser + " has completed an Assignment"),
+        ("The Assignment with " + assignment.videos.length() + " that was assigned ", assignment.dateStart + " has been completed."),
+        "MESSAGE",
+        childUser.id,
+        childUser.id
+    )
+
 }
